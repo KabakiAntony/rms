bd5b9bf033f823935d28a4dc2e299b4e63eeee77
Kabaki Antony <kabaki.antony@gmail.com>
2020-09-30 22:02:20 +0300

    added flask-migrate to handle database migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
-# from config import TestingConfig
+from flask_migrate import Migrate
+
+
+migrate = Migrate()
 
 
 def create_app():
@@ -7,6 +10,10 @@ def create_app():
     this is my app factory
     """
     app = Flask(__name__)
-    app.config.from_object('config.TestingConfig')
+    app.config.from_object('config.DevelopmentConfig')
+
+    from app.api.model.models import db
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     return app
