6bb574d2d012b2fda871940e362529ff1f9f6f07
Kabaki Antony <kabaki.antony@gmail.com>
2020-10-01 13:26:24 +0300

    creating the files model

--- app/api/model/models.py
+++ app/api/model/models.py
@@ -110,7 +110,7 @@ class Employees(db.Model):
     lastname = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120))
     companyId = db.Column(db.Integer, db.ForeignKey('Company.id'))
-    project = db.Column(db.Integer, db.ForeignKey('Project.id'))
+    projectId = db.Column(db.Integer, db.ForeignKey('Project.id'))
 
     def __init__(self, firstname, lastname, email, companyId, projectId):
         """initilizing employees model table items"""
@@ -128,4 +128,65 @@ class Employees(db.Model):
             self.email,
             self.companyId,
             self.project
+        )
+
+
+class Files(db.Model):
+    """this creates the files model"""
+    __tablename__ = "Files"
+
+    id = db.Column(db.Integer, primary_key=True)
+    file_name = db.Column(db.String(120), unique=True, nullable=False)
+    companyId = db.Column(db.Integer, db.ForeignKey('Company.id'))
+    projectId = db.Column(db.Integer, db.ForeignKey('Project.id'))
+    created_by = db.Column(db.Integer, db.ForeignKey('Users.id'))
+    date_created = db.Column(db.DateTime)
+    reviewed_by = db.Column(db.Integer, db.ForeignKey('Users.id'))
+    date_reviewed = db.Column(db.DateTime)
+    authorized_by = db.Column(db.Integer, db.ForeignKey('Users.id'))
+    date_authorized = db.Column(db.DateTime)
+    status = db.Column(db.String(25), nullable=False)
+    url = db.Column(db.String(250), nullable=False)
+
+    def __init__(
+        self,
+        file_name,
+        companyId,
+        projectId,
+        created_by,
+        date_created,
+        date_reviewed,
+        authorized_by,
+        reviewed_by,
+        date_authorized,
+        status,
+        url
+    ):
+        """intilizing files model items"""
+        self.file_name = file_name
+        self.companyId = companyId
+        self.projectId = projectId
+        self.created_by = created_by
+        self.date_created = date_created
+        self.reviewed_by = reviewed_by
+        self.date_reviewed = date_reviewed
+        self.authorized_by = authorized_by
+        self.date_authorized = date_authorized
+        self.status = status
+        self.url = url
+
+    def __repr__(self):
+        """formating the file object for view"""
+        return "{}{}{}{}{}{}{}{}{}{}{}" .format(
+            self.file_name,
+            self.companyId,
+            self.projectId,
+            self.created_by,
+            self.date_created,
+            self.reviewed_by,
+            self.date_reviewed,
+            self.authorized_by,
+            self.date_authorized,
+            self.status,
+            self.url
         )
