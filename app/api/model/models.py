968c868a3c592326e82be394db93d1f06bd05abb
Kabaki Antony <kabaki.antony@gmail.com>
2020-10-20 18:43:42 +0300

    removed user and company model

--- app/api/model/models.py
+++ app/api/model/models.py
@@ -1,49 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
+from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
-
-
-class Company(db.Model):
-    """
-    this creates the company model
-    """
-    __tablename__ = "Company"
-
-    id = db.Column(db.Integer, primary_key=True)
-    company = db.Column(db.String(64), index=True, unique=True)
-    joined_at = db.Column(db.DateTime(), default=datetime.utcnow)
-
-    def __init__(self, company, joined_at):
-        """
-        initializing company db values
-        """
-        self.company = company
-        self.joined_at = joined_at
-
-
-class User(db.Model):
-    """
-    this creates the system users model
-    """
-    __tablename__ = "Users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(64), index=True, unique=False)
-    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    companyId = db.Column(db.Integer, db.ForeignKey('Company.id'))
-    role = db.Column(db.String(25), nullable=False)
-    isActive = db.Column(db.String(25), default='False', nullable=False)
-
-    def __init__(self, firstname, email, password, role, companyId):
-        """initilize user db values"""
-        self.firstname = firstname
-        self.email = email
-        self.password = password
-        self.companyId = companyId
-        self.role = role
+ma = Marshmallow()
 
 
 class Project(db.Model):
