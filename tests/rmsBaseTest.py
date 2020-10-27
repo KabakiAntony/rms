ec9e8b4ed98095d29a66cf7c2ada04d5ee199ef2
Kabaki Antony <kabaki.antony@gmail.com>
2020-10-27 13:17:36 +0300

    made a base test for setUp

---
+++ tests/rmsBaseTest.py
@@ -0,0 +1,15 @@
+# this class will hold my setUp and tearDown
+# methods for all my test modules
+import unittest
+from app import create_app
+from app.api.model.models import db
+
+
+class RmsBaseTest(unittest.TestCase):
+    """create setUp and tearDown for RMS"""
+    def setUp(self):
+        """set up tests all modules"""
+        self.app = create_app()
+        self.app.config.from_object('config.TestingConfig')
+        self.client = self.app.test_client()
+        db.create_all()
