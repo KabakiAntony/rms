95c86013b70e9a82ad46afb045fe6a9064ce562a
Kabaki Antony <kabaki.antony@gmail.com>
2020-11-17 12:08:16 +0300

    cleaned intent file

--- app/static/js/intent.js
+++ app/static/js/intent.js
@@ -2,14 +2,22 @@ import {validateEmailData} from './rmsModule.js'
 import {emailInputListener} from './rmsModule.js'
 import {rmsFetch} from './rmsModule.js'
 
+/* variables for this form */
 const signUpForm = document.getElementById('rmsForm');
 const upEmail = document.getElementById('rmsEmail');
 const username = document.getElementById('rmsUsername');
 const company = document.getElementById('rmsCompany');
-
 let intentData;
 
+/* validate inputs*/
 emailInputListener(upEmail);
+
+/* sign up submission */
+signUpForm.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  validateEmailData();
+  SignUp();
+}); 
 
 /* this function sends data to the server */
 function SignUp(){
@@ -20,10 +28,3 @@ intentData = {
 };
 rmsFetch('/intent',"POST",intentData)
 }
-
-/* sign up submission */
-signUpForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    validateEmailData();
-    SignUp();
-}); 
