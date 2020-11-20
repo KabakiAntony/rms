bf195807feb91438c0591f42836f015b315b40af
Kabaki Antony <kabaki.antony@gmail.com>
2020-11-20 22:30:53 +0300

    removed project model to its own module

--- app/api/model/models.py
+++ app/api/model/models.py
@@ -5,26 +5,6 @@ db = SQLAlchemy()
 ma = Marshmallow()
 
 
-class Project(db.Model):
-    """
-    this creates the project model
-    """
-    __tablename__ = "Project"
-
-    id = db.Column(db.Integer, primary_key=True)
-    project_name = db.Column(db.String(100), nullable=False, unique=True)
-    companyId = db.Column(db.Integer, db.ForeignKey('Company.id'))
-    date_from = db.Column(db.DateTime)
-    date_to = db.Column(db.DateTime)
-
-    def __init__(self, project_name, companyId, date_from, date_to):
-        """initializing inputs for this model"""
-        self.project_name = project_name
-        self.companyId = companyId
-        self.date_from = date_from
-        self.date_to = date_to
-
-
 class Budget(db.Model):
     """
     this creates the  budget model
