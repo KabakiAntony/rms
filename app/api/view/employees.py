49be9b7398cedaf426a5d8e44a725fbe2fceac66
KabakiAntony <kabaki.antony@gmail.com>
2021-04-02 14:43:30 +0300

    added specific error reporting to nofity the user why a certain error occurred

--- app/api/view/employees.py
+++ app/api/view/employees.py
@@ -2,13 +2,14 @@ import os
 from app.api import rms
 from werkzeug.utils import secure_filename
 from flask import request, abort
+import psycopg2
 from app.api.utils import allowed_extension, custom_make_response,\
      token_required, rename_file, add_id_and_company_id,\
      convert_to_csv, insert_csv
 
 
 KEY = os.environ.get('SECRET_KEY')
-EMPLOYEE_UPLOAD_FOLDER = os.environ.get('EMPLOYEES_FOLDER')
+EMPLOYEE_UPLOAD_FOLDER = os.environ.get('EMPLOYEES_FOLDER') 
 
 
 @rms.route('/auth/upload/employees', methods=['POST'])
@@ -47,14 +48,33 @@ def upload_employee_master(user):
                 "Only excel files are allowed !",
                 400
             )
-    except Exception as e:
-        # print(f"{e}")
+    except psycopg2.Error as e:
         # exceptions go to site administrator and email
         # the user gets a friendly error notification
-        abort(
-            custom_make_response(
-                "error",
-                f"The following error occured : '{e}'",
-                400
+        if e.pgcode == ('23505'):
+            abort(
+                custom_make_response(
+                    "error",
+                    "The record(s) you are trying to upload already exist in the database !",
+                    400
+                )
             )
-        )
+        elif e.pgcode == ('22P04'):
+            abort(
+                custom_make_response(
+                    "error",
+                    "The file you are uploading is not in the allowed format !",
+                    400
+                    )
+                )
+        else:
+            abort(
+                custom_make_response(
+                    "error",
+                    f'The following error occured {e}',
+                    400
+                    )
+                )
+
+
+        
