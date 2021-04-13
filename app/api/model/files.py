d3b7662ea6aa7b223e4fe79f58dcab9cd5e3a332
KabakiAntony <kabaki.antony@gmail.com>
2021-04-13 11:11:28 +0300

    description and modification of the files model

--- app/api/model/files.py
+++ app/api/model/files.py
@@ -6,42 +6,42 @@ class Files(db.Model):
     __tablename__ = "Files"
 
     id = db.Column(db.String(20), primary_key=True)
-    fileName = db.Column(db.String(120), unique=True, nullable=False)
+    # fileName = db.Column(db.String(120), unique=True, nullable=False)
     companyId = db.Column(db.String(20), db.ForeignKey('Company.id'))
     projectId = db.Column(db.String(20), db.ForeignKey('Project.id'))
     createdBy = db.Column(db.String(20), db.ForeignKey('Users.id'))
     dateCreated = db.Column(db.DateTime)
-    reviewedBy = db.Column(db.String(20), db.ForeignKey('Users.id'))
-    dateReviewed = db.Column(db.DateTime)
+    # reviewedBy = db.Column(db.String(20), db.ForeignKey('Users.id'))
+    # dateReviewed = db.Column(db.DateTime)
     authorizedBy = db.Column(db.String(20), db.ForeignKey('Users.id'))
-    dateAuthorized = db.Column(db.DateTime)
+    dateAuthorizedOrRejected = db.Column(db.DateTime)
     fileStatus = db.Column(db.String(25), nullable=False)
     fileUrl = db.Column(db.String(250), nullable=False)
 
     def __init__(
         self,
-        fileName,
+        # fileName,
         companyId,
         projectId,
         createdBy,
         dateCreated,
-        dateReviewed,
+        # dateReviewed,
+        # reviewedBy,
         authorizedBy,
-        reviewedBy,
-        dateAuthorized,
+        dateAuthorizedOrRejected,
         fileStatus,
         fileUrl
     ):
         """intilizing files model items"""
-        self.fileName = fileName
+        # self.fileName = fileName
         self.companyId = companyId
         self.projectId = projectId
         self.createdBy = createdBy
         self.dateCreated = dateCreated
-        self.reviewedBy = reviewedBy
-        self.dateReviewed = dateReviewed
+        # self.reviewedBy = reviewedBy
+        # self.dateReviewed = dateReviewed
         self.authorizedBy = authorizedBy
-        self.dateAuthorized = dateAuthorized
+        self.dateAuthorizedOrRejected = dateAuthorizedOrRejected
         self.fileStatus = fileStatus
         self.fileUrl = fileUrl
 
@@ -50,15 +50,15 @@ class FilesSchema(ma.Schema):
     class Meta:
         files = (
             "id",
-            "fileName",
+            # "fileName",
             "companyId",
             "projectid",
             "createdBy",
             "dateCreated",
-            "reviewedBy",
-            "dateReviewed",
+            # "reviewedBy",
+            # "dateReviewed",
             "authorizedBy",
-            "dateAuthorized",
+            "dateAuthorizedOrRejected",
             "fileStatus",
             "fileUrl"
         )
