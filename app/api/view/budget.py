622f01db2dcdccef4556c6c1523ff0572d26852b
KabakiAntony <kabaki.antony@gmail.com>
2021-04-21 17:10:41 +0300

    refactored budget file uploads view

--- app/api/view/budget.py
+++ app/api/view/budget.py
@@ -11,6 +11,7 @@ from flask import request, abort
 from app.api.utils import allowed_extension, custom_make_response,\
      token_required, rename_file, add_id_and_company_id,\
      insert_csv, convert_to_csv, generate_db_ids, check_for_whitespace
+from .files import insert_file_data, file_operation
 
 
 # getting environment variables
@@ -28,35 +29,19 @@ def upload_budget(user):
     if (user['role'] != "Creator"):
         abort(custom_make_response(
             "error",
-            "You are not authorized to carry out this action!",
+            "You are not authorized to carry out this action.",
             403
         ))
     try:
         receivedFile = request.files['budgetExcelFile']
         if allowed_extension(receivedFile.filename) and receivedFile:
-            secureFilename = secure_filename(receivedFile.filename)
-            filePath = os.path.join(BUDGET_UPLOAD_FOLDER, secureFilename)
-            receivedFile.save(filePath)
-            new_file_path = rename_file(
-                filePath,
-                user['companyId'],
-                BUDGET_UPLOAD_FOLDER,
-                '_budget_'
-            )
-            csvFile = convert_to_csv(new_file_path, BUDGET_UPLOAD_FOLDER)
-            budget_amount = get_budget_amount(csvFile)
-            insert_budget_data(user['companyId'], budget_amount, new_file_path)
-            return custom_make_response(
-                "data",
-                "File uploaded successfully.",
-                200
-                )
-        else:
-            return custom_make_response(
-                "error",
-                "Only excel files are allowed !",
-                400
-            )
+            return file_operation(receivedFile,BUDGET_UPLOAD_FOLDER,'Budget',user['companyId'],user['id'])
+        return custom_make_response(
+            "error",
+            "Only excel files are allowed, please select a budget excel file & try again.",
+            400
+        )
+
     except Exception as e:
         # exceptions go to site administrator and email
         # the user gets a friendly error notification
@@ -65,7 +50,8 @@ def upload_budget(user):
             abort(
                 custom_make_response(
                     "error",
-                    "The file you are uploading is not in the allowed format for a budget file !",
+                    "The file you are uploading is not in the allowed format\
+                         for a budget file, please check and try again.",
                     400
                 )
             )
@@ -73,7 +59,7 @@ def upload_budget(user):
             abort(
                 custom_make_response(
                     "error",
-                    "Please select the project you are uploading a budget for !",
+                    "Please select the project you are uploading a budget for.",
                     400
                 )
             )
@@ -85,49 +71,11 @@ def upload_budget(user):
                     400
                 )
             )
-       
+    
 
 
-def get_budget_amount(budget_file_csv):
-    """
-    get the budget amount from
-    from the just uploaded file
-    """
-    saved_csv = open(budget_file_csv, "r")
-    reader_file = csv.reader(saved_csv)
-    count_rows = 0
-    for row in reader_file:
-        count_rows += 1
-    budget_amount = row[1]
-    return budget_amount
 
+    
 
-def insert_budget_data(company_id, budgetAmt, fileUrl):
-    """
-    insert budget data into the budget
-    table for the authorizer
-    to act accordingly
-    """
-    project_name = request.form['projectName']
-    projectName = project_name + '.' + company_id
-    this_project = Project.query.\
-        filter_by(project_name=projectName).first()
-    project = project_schema.dump(this_project)
-    project_id = project['id']
-    id = generate_db_ids()
-    companyId = company_id
-    amount = budgetAmt
-    fileUrl = fileUrl
-
-    new_budget = Budget(
-        id=id,
-        companyId=companyId,
-        projectId=project_id,
-        amount=amount,
-        fileUrl=fileUrl
-        )
-    # insert the data into the db
-    db.session.add(new_budget)
-    db.session.commit()
     
 
