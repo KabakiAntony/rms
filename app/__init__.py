f69cfc18b6534c2a9ef1c3c9afe00bf9ea6558fa
KabakiAntony <kabaki.antony@gmail.com>
2021-04-21 17:07:25 +0300

    registered the payments blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ from app.api.view.project import rms as project_blueprint
 from app.api.view.budget import rms as budget_blueprint
 from app.api.view.employees import rms as employee_blueprint
 from app.api.view.user import rms as user_blueprint
-from app.api.view.files import rms as files_blueprint
+from app.api.view.payments import rms as payment_blueprint
 
 
 migrate = Migrate(compare_type=True)
@@ -36,7 +36,7 @@ def create_app():
     app.register_blueprint(budget_blueprint)
     app.register_blueprint(employee_blueprint)
     app.register_blueprint(user_blueprint)
-    app.register_blueprint(files_blueprint)
+    app.register_blueprint(payment_blueprint)
     app.app_context().push()
 
     return app
