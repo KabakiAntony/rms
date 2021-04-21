f65ad724a21f81734760f66e268abe69ac533e3f
KabakiAntony <kabaki.antony@gmail.com>
2021-04-21 12:06:39 +0300

    added a way to get projects and cascade them depending on the tab that is open

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -11,4 +11,58 @@ function openAction(evt, actionName) {
     document.getElementById(actionName).style.display = "block";
     document.getElementById('profileInstruction').style.display = "none";
     evt.currentTarget.className += " active";
+    
+    if (actionName === "UploadPayment" || actionName === "UploadBudget"){
+        getProject(actionName);
+    }
   }
+
+
+let budgetDropDown = document.getElementById('rmsCreatorBudgetTag');
+let paymentDropDown = document.getElementById('rmsCreatorPaymentTag');
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Select project';
+let companyId = document.getElementById('rmsCreatorBudgetCompanyId');
+
+// we are hooking the fetch projects to the tab the user is on
+// determine the the open tab then cascade the  dropdown accordingly
+
+function getProject(theTab){
+    let dropDown = null;
+
+    if (theTab === "UploadPayment"){
+        dropDown = paymentDropDown;
+    }
+    else{
+        dropDown = budgetDropDown;
+    }
+
+    dropDown.length = 0 ;
+    dropDown.add(defaultOption);
+    dropDown.selectedIndex = 0;
+
+    const url = 'projects/name/'+companyId.value;
+    fetch(url,{
+            method: "GET"
+        })
+        .then(response => response.json())
+        .then(({data,status,error})=>{
+            let option;
+            let name_only;
+            if (status == 200)
+            {
+                for (let i = 0; i < data.length; i++) {
+                    option = document.createElement('option');
+                    name_only = data[i].project_name.split('.')[0] 
+                    option.text = name_only;
+                    dropDown.add(option);
+                }
+            }
+            else
+            {
+                console.log(`an error occured ${error}`)
+            }
+
+        })
+    .catch(err => console.log(`This error occured :${err}`));
+}
