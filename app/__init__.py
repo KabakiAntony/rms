e37ef7bc7936dd2c0a47e6f63158384af002f571
KabakiAntony <kabaki.antony@gmail.com>
2021-04-27 13:43:27 +0300

    formattting using black for cleaner code

--- app/__init__.py
+++ app/__init__.py
@@ -22,9 +22,10 @@ def create_app():
     this is my app factory
     """
     app = Flask(__name__)
-    app.config.from_object('config.DevelopmentConfig')
+    app.config.from_object("config.DevelopmentConfig")
 
     from app.api.model import db, ma
+
     db.init_app(app)
     ma.init_app(app)
     migrate.init_app(app, db)
