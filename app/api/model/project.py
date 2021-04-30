d43300f04920b7f8a27df4047b94f4e91ecf3c59
KabakiAntony <kabaki.antony@gmail.com>
2021-04-30 13:43:42 +0300

    added a project status column

--- app/api/model/project.py
+++ app/api/model/project.py
@@ -13,19 +13,23 @@ class Project(db.Model):
     companyId = db.Column(db.String(20), db.ForeignKey("Company.id"))
     date_from = db.Column(db.Date)
     date_to = db.Column(db.Date)
+    project_status = db.Column(db.String(10), nullable=False)
 
-    def __init__(self, id, project_name, companyId, date_from, date_to):
+    def __init__(self, id, project_name, companyId, date_from, date_to, project_status):
         """initializing inputs for this model"""
         self.id = id
         self.project_name = project_name
         self.companyId = companyId
         self.date_from = date_from
         self.date_to = date_to
+        self.project_status = project_status
 
 
 class ProjectSchema(ma.Schema):
     class Meta:
-        fields = ("id", "project_name", "companyId", "date_from", "date_to")
+        fields = (
+            "id", "project_name", "companyId",
+            "date_from", "date_to", "project_status")
 
 
 project_schema = ProjectSchema()
