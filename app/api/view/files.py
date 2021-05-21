912c89727ddc449a61b80fec5aef402b8549c949
KabakiAntony <kabaki.antony@gmail.com>
2021-05-21 16:50:33 +0300

    added a filter to get only the pending files for the authorizer

--- app/api/view/files.py
+++ app/api/view/files.py
@@ -278,13 +278,14 @@ def get_authorizer_files(user, companyId, fileType):
     file_data = db.session.query(Files, Project).\
         filter(Files.projectId == Project.id).\
         filter_by(companyId=user['companyId']).\
+        filter_by(fileStatus="Pending").\
         filter_by(fileType=fileType.capitalize()).all()
 
     if not file_data:
         abort(
             custom_make_response(
                 "error",
-                f"No {fileType} file(s) have been found for your company,\
+                f"No unauthorized  {fileType}  file(s) have been found for your company,\
                      ask the creator to upload & try again.", 404
             )
         )
@@ -297,7 +298,7 @@ def get_authorizer_files(user, companyId, fileType):
             "project_name": result[1].project_name,
             "fileStatus": result[0].fileStatus,
             "fileAmount": result[0].fileAmount,
-            "dateCreated": result[0].dateCreated.strftime('%m-%d-%Y'),
+            "dateCreated": result[0].dateCreated.strftime("%Y-%m-%d"),
             "fileName": result[0].fileName
         }
         fileList.append(result_format)
@@ -339,7 +340,7 @@ def authorize_or_reject(user, id):
         Files.query.filter_by(id=fileId).update(new_file_params)
         db.session.commit()
         return custom_make_response(
-            "data", f"File {file_status} successfully.", 200)
+            "data", f"File : {fileId} {file_status} successfully.", 200)
 
     except Exception as e:
         abort(
