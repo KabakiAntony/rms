44cbfbefc7843dd86f31737f69ef855eff4e5ac1
KabakiAntony <kabaki.antony@gmail.com>
2021-05-21 16:52:22 +0300

    added functionality for rejecting files

--- app/static/js/authorizerActions.js
+++ app/static/js/authorizerActions.js
@@ -7,31 +7,56 @@ let project_files = document.getElementById('show_files');
 let fileId = document.getElementById('rmsFileId');
 let authorizeButton = document.getElementById('authorizeFile');
 let rejectButton = document.getElementById("rejectFile");
-let authorizeBody = "";
+let authorizeBody = "",  rejectBody="";
 
 selectFileType.addEventListener('change',()=>{
-    showLoader();
-    project_files.innerHTML = "";
-    let filesUrl = `/auth/files/${companyId.value}/${selectFileType.value}`;
-    rmsFetchGet(filesUrl,"",listFiles)
+    if (selectFileType.value != "none"){
+        showLoader();
+        project_files.innerHTML = "";
+        let filesUrl = `/auth/files/${companyId.value}/${selectFileType.value}`;
+        rmsFetchGet(filesUrl,"",listFiles);
+    }
 })
+
+function show_error_notification(errorMsg){
+    document.getElementById('error').innerHTML = `${errorMsg}`;
+    document.getElementById('error').style.display = " block";
+    setTimeout(function(){ 
+        document.getElementById('error').style.display = "none";
+    }, 10000);
+    
+}
+function authorization_rejection_action(actionBody){
+    if (fileId.value ===""){
+        let error = "Select the file you want to authorize on the table";
+        show_error_notification(error);
+    }
+    else {
+        showLoader();
+        let patch_url = `/action/files/${fileId.value}`;
+        rmsFetch(patch_url,'PATCH',actionBody,'','')
+        fileId.value = "";
+        remove_row_highlight();
+        setTimeout(()=>{ 
+            project_files.innerHTML="";
+            selectFileType.selectedIndex = 0;
+        },10000);
+    }
+}
 
 authorizeButton.addEventListener('click', ()=>{
     authorizeBody = {
         new_status:"Authorized",
     }
-    if (fileId.value ==""){
-        alert("Select the file you want to authorize.");
-    }
-    else{
-        showLoader();
-        let patch_url = `/action/files/${fileId.value}`;
-        rmsFetch(patch_url,'PATCH',authorizeBody,'','')
-        fileId.value = "";
-        remove_row_highlight();
-    }
+    authorization_rejection_action(authorizeBody);
 })
 
+rejectButton.addEventListener('click', ()=>{
+    rejectBody = {
+        new_status:"Rejected",
+    }
+    authorization_rejection_action(rejectBody);
+})
 
 
 
